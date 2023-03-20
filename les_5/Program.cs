48416af4aaa9ac76383d1904e80f064773167404
марта <kukovamk@gmail.com>
2023-03-20 20:07:20 +0300

    les_5

--- les_5/Program.cs
+++ les_5/Program.cs
@@ -1,6 +1,33 @@
 ﻿// 4. Задайте двумерный массив. Введите элемент, и найдите первое его вхождение,
 //    выведите позиции по горизонтали и вертикали, или напишите, что такого элемента нет.
 
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+
+
 string SearchNum(int[,] arr, int num)
 {
     int row = arr.GetLength(0);
