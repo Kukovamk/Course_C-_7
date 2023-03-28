c1a97890873faeb57bc852c6ea5e563ced097ed9
марта <kukovamk@gmail.com>
2023-03-28 20:23:39 +0300

    HW_1

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задайте двумерный массив размером m*n, заполненный
 // случайными вещественными числами. 
 
-void Print(double[,] arr)
+void Print(int[,] arr)
 {
     int row = arr.GetLength(0);
     int column = arr.GetLength(1);
@@ -16,18 +16,19 @@ void Print(double[,] arr)
 
 double[,] MassNums(double row, double column, double from, double to)
 {
-    double[,] arr = new double[row, column];
+    double[,] arr = new double[(int)row, (int)column];
 
     for (int i = 0; i < row; i++)
         for (int j = 0; j < column; j++)
-            arr[i, j] = Convert.ToDouble(new Random().Next(from, to));
+            arr[i, j] = Convert.ToDouble(new Random().Next((int)from, (int)to));
     return arr;
 }
 
 int row_num = Convert.ToInt32(Console.ReadLine()!);
-int column_num =Convert.ToInt32(Console.ReadLine()!);
-int start =Convert.ToInt32(Console.ReadLine()!);
+int column_num = Convert.ToInt32(Console.ReadLine()!);
+int start = Convert.ToInt32(Console.ReadLine()!);
 int stop = Convert.ToInt32(Console.ReadLine()!);
 
 double[,] mass = MassNums(row_num, column_num, start, stop);
-Print(mass);
+Console.WriteLine();
+
